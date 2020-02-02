045ded8ce80a3855087b28a367194d73bc0e81f4
Taylor Laekeman <taylor.laekeman@gmail.com>
2020-02-02 17:35:56 -0500

    add first models

---
+++ orders/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from .models import Order, OrderType
+from .utils.create_order_item import create_order_item
+
+
+class OrderItemSerializer(serializers.Serializer):
+    order_type = serializers.CharField()
+    ideas = serializers.CharField(required=False)
+
+
+class OrderSerializer(serializers.Serializer):
+    contact = serializers.EmailField()
+    destination = serializers.CharField()
+    items = serializers.ListField(child=OrderItemSerializer())
+
+    def create(self, validated_data):
+        order = Order.objects.create(
+            contact=validated_data['contact'],
+            destination=validated_data['destination'],
+        )
+        items = []
+        for item in validated_data['items']:
+            items.append(create_order_item(order, item))
+        return order
+
+
+class OrderTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = OrderType
+        fields = ['name']
