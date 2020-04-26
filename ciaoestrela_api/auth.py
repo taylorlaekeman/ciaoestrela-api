8e9e98f282d9238b72fec695d00213a017fe935f
taylor <taylor.laekeman@gmail.com>
2020-04-26 15:34:30 -0400

    add oidc authentication with okta

---
+++ ciaoestrela_api/auth.py
@@ -0,0 +1,26 @@
+import logging
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+import requests
+
+from ciaoestrela_api.settings import OKTA_CLIENT_ID, OKTA_CLIENT_SECRET
+
+logger = logging.getLogger(__name__)
+
+AUTH = (OKTA_CLIENT_ID, OKTA_CLIENT_SECRET)
+
+
+class AuthenticatedUser():
+    def is_authenticated(self):
+        pass
+
+
+class OktaAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token = request.META.get('HTTP_AUTHORIZATION').split(' ')[-1]
+        data = {'token': token}
+        response = requests.post('https://dev-777810.okta.com/oauth2/default/v1/introspect', auth=AUTH, data=data)
+        response_json = response.json()
+        if not response_json['active']:
+            raise AuthenticationFailed('authentication failed')
+        return (AuthenticatedUser(), None)
